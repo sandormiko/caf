c4ac5e841b10159000805ec708be8e3d9530dcad
Sándor Mikó <miko@elte.hu>
2017-06-09 23:15:10 +0200

    Formatting

--- caf-foundation/src/main/java/caf/foundation/kafka/KafkaProducerProvider.java
+++ caf-foundation/src/main/java/caf/foundation/kafka/KafkaProducerProvider.java
@@ -13,32 +13,27 @@ import com.google.inject.Provider;
 import caf.foundation.config.ApplicationConfiguration;
 import caf.foundation.config.ProducerFactory;
 
-
 @Singleton
-public class KafkaProducerProvider implements Provider<Producer<String,byte[]>>{
+public class KafkaProducerProvider implements Provider<Producer<String, byte[]>> {
 
 	private final ProducerFactory producerConfig;
-	
+
 	@Inject
-	public KafkaProducerProvider(ApplicationConfiguration appConfig){
+	public KafkaProducerProvider(ApplicationConfiguration appConfig) {
 		this.producerConfig = appConfig.getProducer();
 	}
-	
+
 	@Override
 	public Producer<String, byte[]> get() {
 		Properties props = new Properties();
-        props.put("bootstrap.servers", producerConfig.getBootStrapServers());
-       
-        props.put("enable.auto.commit", producerConfig.getEnableAutoCommit().toString());
-        props.put("auto.commit.interval.ms", producerConfig.getAutoCommitInterval());
-        
-        props.put("key.serializer", org.apache.kafka.common.serialization.StringSerializer.class.getName());
-        props.put("value.serializer", org.apache.kafka.common.serialization.ByteArraySerializer.class.getName());
-        
+		props.put("bootstrap.servers", producerConfig.getBootStrapServers());
+		props.put("enable.auto.commit", producerConfig.getEnableAutoCommit());
+		props.put("auto.commit.interval.ms", producerConfig.getAutoCommitInterval());
+		props.put("key.serializer", org.apache.kafka.common.serialization.StringSerializer.class.getName());
+		props.put("value.serializer", org.apache.kafka.common.serialization.ByteArraySerializer.class.getName());
 
-        Producer<String,byte[]> producer = new KafkaProducer<String,byte[]>(props);
-        
-		return producer;
+		return new KafkaProducer<String, byte[]>(props);
+		
 	}
 
 }
