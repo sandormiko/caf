e3a262d5eb47fee33c039fb384a7c12d3d2f825d
Sándor Mikó <miko@elte.hu>
2017-09-06 22:47:00 +0200

    Cosmetics

--- caf-admin/src/test/java/caf/admin/employees/boundary/EmployeeResourceTest.java
+++ caf-admin/src/test/java/caf/admin/employees/boundary/EmployeeResourceTest.java
@@ -5,34 +5,24 @@ import caf.admin.employee.matcher.EmployeeMatcher;
 import caf.admin.employees.control.EmployeeBuilder;
 import caf.admin.employees.control.EmployeeDAO;
 import caf.admin.employees.entity.Employee;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.fakemongo.Fongo;
-import com.github.fakemongo.junit.FongoRule;
-import com.mongodb.DBObject;
-import com.mongodb.util.JSON;
 import io.dropwizard.testing.junit.ResourceTestRule;
-import org.assertj.core.util.Lists;
-import org.eclipse.jetty.http.HttpStatus;
-import org.hamcrest.Matchers;
-import org.hamcrest.core.Is;
-import org.junit.*;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 
-import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Response;
-
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 
 import static javax.ws.rs.client.Entity.json;
 import static org.eclipse.jetty.http.HttpStatus.CREATED_201;
 import static org.eclipse.jetty.http.HttpStatus.OK_200;
 import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -41,18 +31,22 @@ public class EmployeeResourceTest {
 
     private static Fongo fongo = null;
 
-    private static EmployeeDAO setupDAO() {
+    @BeforeClass
+    public static void initFongo(){
         fongo = new Fongo("inMemoryServer");
+    }
+
+
+    @Rule
+    public final ResourceTestRule resources = ResourceTestRule.builder()
+            .addResource(new EmployeeResource(setupEmployeeDAO())).build();
+
+    private EmployeeDAO setupEmployeeDAO() {
         Morphia morphia = new Morphia();
         Datastore ds = morphia.createDatastore(fongo.getMongo(), "test");
         return new EmployeeDAO(ds);
 
     }
-
-
-    @ClassRule
-    public static final ResourceTestRule resources = ResourceTestRule.builder()
-            .addResource(new EmployeeResource(setupDAO())).build();
 
     @After
     public void after() {
@@ -91,7 +85,6 @@ public class EmployeeResourceTest {
     }
 
     private List<Employee> employeesRetrieved(Response response) {
-        return response.readEntity(new GenericType<List<Employee>>() {
-        });
+        return response.readEntity(new GenericType<List<Employee>>() {});
     }
 }
